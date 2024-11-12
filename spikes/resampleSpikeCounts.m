cb3575294ba274b1058dc5ee2e5242df1dbe7892
dervinism <martynas.dervinis@gmail.com>
2024-11-12 12:52:40 +0200

    Regular commit 12/11/2024

--- spikes/resampleSpikeCounts.m
+++ spikes/resampleSpikeCounts.m
@@ -54,4 +54,4 @@ for s = 1:newBiggestIndex
 end
 
 % Calculate time bins
-timeBins = (1:size(resampledSpikeCounts,2)).*options.newStepsize;
+timeBins = (1:size(resampledSpikeCounts,2)).*options.newStepsize - options.newStepsize/2;
